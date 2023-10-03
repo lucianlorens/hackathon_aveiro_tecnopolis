b65e42dfa6714bfd12b53cbb89bef9eb91ff8e3a
lucianlorens <lucianlorens@gmail.com>
2023-10-03 17:11:48 +0100

    :tada: hello world streamlit!

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+from matplotlib import container
+import streamlit as st
+import pandas as pd
+
+import util as util
+
+# st.set_page_config(page_title='LCA Calculator', page_icon = 'streamlit_app/favicon.png', layout = 'wide', initial_sidebar_state = 'auto')
+st.title('Tecnopolis!')
