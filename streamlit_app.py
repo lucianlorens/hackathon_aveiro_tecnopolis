0b8a95f20b2ef27fd466db6e8bf5a221004d6741
lucianlorens <lucianlorens@gmail.com>
2023-10-04 16:13:18 +0100

    :tada: more maaapsgit add .

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import folium
 import matplotlib.pyplot as plt
 from datetime import *
 
+import util.map_plotter as map_plotter
+
 import pandas as pd
 
 st.title('Tecnopolis')
@@ -55,104 +57,8 @@ filtered_df = find_rows_by_timestamp(df_gold, selected_datetime)
 # Function to create a map using Folium
 data = filtered_df
 
-m = folium.Map(location=[data['latitude'].mean(), data['longitude'].mean()], zoom_start=15)
 
-# Function to assign marker color based on pollution level
-def get_marker_color(pollution_value):
-    if pollution_value <= 50:
-        return 'green'
-    elif pollution_value <= 100:
-        return 'yellow'
-    elif pollution_value <= 150:
-        return 'orange'
-    else:
-        return 'red'
-
-for index, row in data.iterrows():
-
-    #add carbon dioxide
-    folium.CircleMarker(
-        location=[row['latitude'], row['longitude']],
-        radius=5 * row['carbon_dioxide (ppm)'] / 100,  # Adjust marker size based on CO2 level
-        # color=get_marker_color(row['carbon_dioxide (ppm)']),  # Color based on CO2 level
-        color= 'blue ',  # Color based on CO2 level
-        fill=True,
-        # fill_color=get_marker_color(row['carbon_dioxide (ppm)']),
-        fill_color='blue',
-        fill_opacity=0.2
-    ).add_to(m)
-
-    folium.CircleMarker(
-        location=[row['latitude'], row['longitude']],
-        radius=5 * row['carbon_monoxide (ug/m3)'] / 100,  # Adjust marker size based on CO2 level
-        # color=get_marker_color(row['carbon_dioxide (ppm)']),  # Color based on CO2 level
-        color= 'red ',  # Color based on CO2 level
-        fill=True,
-        # fill_color=get_marker_color(row['carbon_dioxide (ppm)']),
-        fill_color='red',
-        fill_opacity=0.2
-    ).add_to(m)
-
-
-    folium.CircleMarker(
-        location=[row['latitude'], row['longitude']],
-        radius=5 * row['nitrogen_dioxide (ug/m3)'] / 100,  # Adjust marker size based on CO2 level
-        # color=get_marker_color(row['carbon_dioxide (ppm)']),  # Color based on CO2 level
-        color= 'green ',  # Color based on CO2 level
-        fill=True,
-        # fill_color=get_marker_color(row['carbon_dioxide (ppm)']),
-        fill_color='green',
-        fill_opacity=0.2
-    ).add_to(m)
-
-st_folium(m, width=800)
-
-
-### BAR CHART 
-# data = data.dropna(subset=['carbon_dioxide (ppm)', 'carbon_monoxide (ug/m3)', 'nitrogen_dioxide (ug/m3)'])
-
-# # Select relevant columns
-# pollution_data = data[['carbon_dioxide (ppm)', 'carbon_monoxide (ug/m3)', 'nitrogen_dioxide (ug/m3)']]
-
-# # Calculate the mean pollution values
-# mean_pollution = pollution_data.mean()
-
-# # Create a bar graph
-# plt.figure(figsize=(10, 6))
-# mean_pollution.plot(kind='bar', color=['blue', 'green', 'red'])
-# plt.title('Mean Pollution Levels')
-# plt.xlabel('Pollutant')
-# plt.ylabel('Mean Level')
-# plt.xticks(rotation=0)
-# # st.pyplot(mean_pollution)
-
-# import streamlit as st
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# # Load the data from the provided CSV
-# # data = pd.read_csv('path/to/your/csv.csv')
-
-# # Filter out rows with missing pollution data
-# data = data.dropna(subset=['carbon_dioxide (ppm)', 'carbon_monoxide (ug/m3)', 'nitrogen_dioxide (ug/m3)'])
-
-# # Select relevant columns
-# pollution_data = data[['carbon_dioxide (ppm)', 'carbon_monoxide (ug/m3)', 'nitrogen_dioxide (ug/m3)']]
-
-# # Calculate the mean pollution values
-# mean_pollution = pollution_data.mean()
-
-# # Display the bar graph using Streamlit
-# st.bar_chart(mean_pollution)
-
-# # Optionally, display the numerical values as well
-# st.write('Mean Pollution Levels:')
-# st.write(mean_pollution)
-
-### create 3D map
-
-# =====
-
-import util.map_plotter as map_plotter
+map_plotter.plot_interactivemap(data)
 map_plotter.plot_3D_scatterplot(data)
 map_plotter.heat_map(data)
+map_plotter.bar_chart(data)
--- util/map_plotter.py
+++ util/map_plotter.py
@@ -1,8 +1,68 @@
 import pandas as pd
 import pydeck as pdk
 import streamlit as st
+import matplotlib as plt
+import folium
+from streamlit_folium import st_folium
+import folium
 
-# Load the data from the provided CSV
+def plot_interactivemap(data):
+
+    m = folium.Map(location=[data['latitude'].mean(), data['longitude'].mean()], zoom_start=15)
+
+    # Function to assign marker color based on pollution level
+    def get_marker_color(pollution_value):
+        if pollution_value <= 50:
+            return 'green'
+        elif pollution_value <= 100:
+            return 'yellow'
+        elif pollution_value <= 150:
+            return 'orange'
+        else:
+            return 'red'
+
+    for index, row in data.iterrows():
+
+        #add carbon dioxide
+        folium.CircleMarker(
+            location=[row['latitude'], row['longitude']],
+            radius=5 * row['carbon_dioxide (ppm)'] / 100,  # Adjust marker size based on CO2 level
+            # color=get_marker_color(row['carbon_dioxide (ppm)']),  # Color based on CO2 level
+            color= 'blue ',  # Color based on CO2 level
+            fill=True,
+            # fill_color=get_marker_color(row['carbon_dioxide (ppm)']),
+            fill_color='blue',
+            fill_opacity=0.2
+        ).add_to(m)
+
+        folium.CircleMarker(
+            location=[row['latitude'], row['longitude']],
+            radius=5 * row['carbon_monoxide (ug/m3)'] / 100,  # Adjust marker size based on CO2 level
+            # color=get_marker_color(row['carbon_dioxide (ppm)']),  # Color based on CO2 level
+            color= 'red ',  # Color based on CO2 level
+            fill=True,
+            # fill_color=get_marker_color(row['carbon_dioxide (ppm)']),
+            fill_color='red',
+            fill_opacity=0.2
+        ).add_to(m)
+
+
+        folium.CircleMarker(
+            location=[row['latitude'], row['longitude']],
+            radius=5 * row['nitrogen_dioxide (ug/m3)'] / 100,  # Adjust marker size based on CO2 level
+            # color=get_marker_color(row['carbon_dioxide (ppm)']),  # Color based on CO2 level
+            color= 'green ',  # Color based on CO2 level
+            fill=True,
+            # fill_color=get_marker_color(row['carbon_dioxide (ppm)']),
+            fill_color='green',
+            fill_opacity=0.2
+        ).add_to(m)
+
+    st_folium(m, width=800)
+
+
+
+
 
 def plot_3D_scatterplot(data):
     # Filter out rows with missing pollution data
@@ -81,3 +141,46 @@ def heat_map(data):
 
     # Render the map using Streamlit
     st.pydeck_chart(deck)
+
+
+def bar_chart(data):
+    ## BAR CHART 
+    data = data.dropna(subset=['carbon_dioxide (ppm)', 'carbon_monoxide (ug/m3)', 'nitrogen_dioxide (ug/m3)'])
+
+    # Select relevant columns
+    pollution_data = data[['carbon_dioxide (ppm)', 'carbon_monoxide (ug/m3)', 'nitrogen_dioxide (ug/m3)']]
+
+    # Calculate the mean pollution values
+    mean_pollution = pollution_data.mean()
+
+    # Create a bar graph
+    plt.figure(figsize=(10, 6))
+    mean_pollution.plot(kind='bar', color=['blue', 'green', 'red'])
+    plt.title('Mean Pollution Levels')
+    plt.xlabel('Pollutant')
+    plt.ylabel('Mean Level')
+    plt.xticks(rotation=0)
+    # st.pyplot(mean_pollution)
+
+    import streamlit as st
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    # Load the data from the provided CSV
+    # data = pd.read_csv('path/to/your/csv.csv')
+
+    # Filter out rows with missing pollution data
+    data = data.dropna(subset=['carbon_dioxide (ppm)', 'carbon_monoxide (ug/m3)', 'nitrogen_dioxide (ug/m3)'])
+
+    # Select relevant columns
+    pollution_data = data[['carbon_dioxide (ppm)', 'carbon_monoxide (ug/m3)', 'nitrogen_dioxide (ug/m3)']]
+
+    # Calculate the mean pollution values
+    mean_pollution = pollution_data.mean()
+
+    # Display the bar graph using Streamlit
+    st.bar_chart(mean_pollution)
+
+    # Optionally, display the numerical values as well
+    st.write('Mean Pollution Levels:')
+    st.write(mean_pollution)
