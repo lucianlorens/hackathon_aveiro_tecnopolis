b02c3cd81d1764c4161699fc800c727943866c34
lucianlorens <lucianlorens@gmail.com>
2023-10-04 10:49:33 +0100

    :sparkles: creating datetime seelctor

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,37 @@
 import streamlit as st
 from streamlit_folium import st_folium
 import folium
-
+from datetime import datetime, timedelta
+from datetime import time as dt_time
 
 st.title('Tecnopolis!')
 
+
+
+# Criar slider de data 
+reading_date = st.slider(
+    "Data da Leitura",
+    #min date
+    #max date
+    value=datetime(2020, 1, 1),
+    format="DD/MMMM/YYYY")
+st.write("Data selecionada:", reading_date)
+
+min_datetime = datetime.strptime('00:00', '%H:%M')
+max_datetime = datetime.strptime('23:45', '%H:%M')
+
+
+# Criar slider de hora
+reading_time = st.slider(
+    "Horário da Leitura:",
+    min_value=min_datetime,
+    max_value=max_datetime,
+    value=dt_time(9, 30),
+    # value=(min_datetime, max_datetime),
+    step=timedelta(minutes=15),
+    format="hh:mm"
+    )
+st.write("Hora selecionada:", reading_time)
 
 # Sample data (latitude, longitude, pollution value)
 pollution_data = [(40.64435, -8.64066, 50),
@@ -16,6 +43,8 @@ pollution_data = [(40.64435, -8.64066, 50),
 def create_map(data):
     map = folium.Map(location=[40.64435, -8.64066], zoom_start=12)
     
+    # add points according to filters. 
+
     for item in data:
         lat, lon, pollution_value = item
         folium.CircleMarker(
@@ -23,8 +52,13 @@ def create_map(data):
             radius=pollution_value / 10,  # Adjust marker size based on pollution value
             color='red',
             fill=True,
-            fill_color='red',
+            # vermelho para Monóxido de Carbono, Azul para Dióxido de Carbono,
+            # verde para nitrogênio
+            # pontilhado para poeira
+
+            fill_color='red', 
             fill_opacity=0.6
+
         ).add_to(map)
     
     return map
@@ -39,3 +73,5 @@ map = create_map(pollution_data)
 # folium_static(map)  # Using folium_static to display Folium map in Streamlit
 st_folium(map, width=800)
 
+
+# criar checkbox de quais dados vão ser processados ? 
